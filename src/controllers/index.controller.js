9df84169bdf3e6ee7ed5967641e4f58a5668b859
nylander26 <wolnylander@example.com>
2023-05-01 23:22:55 +0200

    Add JWT

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -1,10 +1,48 @@
-const axios = require('axios');
-const User = require('../models/users');
+const axios = require("axios");
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
 
-const indexCtrl = {}
+const indexCtrl = {};
 
 indexCtrl.renderIndex = (req, res) => {
-    res.send("hola mundo");
+  res.send("hola mundo");
+};
+
+indexCtrl.renderLogin = (req, res) => {
+  const user = {
+    id: 1,
+    name: "July",
+    email: "julio@gmail.com",
+  }
+
+  jwt.sign({ user }, 'secretKey', (err, token) => {
+    res.json(token)
+  })
+}
+
+indexCtrl.renderHome = (req, res) => {
+  jwt.verify(req.token, 'secretKey', (err, authData) => {
+    if (err) {
+      res.status(403).send("Forbidden");
+    } else {
+      res.json({
+        message: "Home",
+        authData
+      })
+    }
+  })
+};
+
+indexCtrl.verifyToken = (req, res, next) => {
+  const token = req.headers.authorization;
+
+  if (typeof token !== 'undefined'){
+    const bearerToken = token.split(" ")[1];
+    req.token = bearerToken
+    next();
+  } else {
+    res.status(403);
+  }
 }
 
 module.exports = indexCtrl;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,7 +1,11 @@
 const { Router } = require('express');
 const router = Router();
-const { renderIndex } = require('../controllers/index.controller')
+const { renderIndex, renderLogin, renderHome, verifyToken } = require('../controllers/index.controller')
 
 router.get('/', renderIndex);
+
+router.post('/api/login', renderLogin)
+
+router.post('/api/home', verifyToken, renderHome);
 
 module.exports = router;
